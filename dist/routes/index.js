c99698d61b200edc401ceadf6a472d66484bb882
Andrey Andrusenko <aandrusenko@yandex.ru>
2026-07-07 10:52:23 +0300

    dist files have been removed from repo

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express = require("express");
-exports.router = express.Router();
-/* GET home page. */
-exports.router.get('/', function (req, res, next) {
-    res.render('index', { title: 'REST MongoDB Server (Express)' });
-});
--- dist/routes/rtq-routtes copy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const jwt_module_1 = require("../auth/jwt-module");
-exports.router = (0, express_1.Router)();
-exports.router.get('/', jwt_module_1.verifyAccess, ((req, res) => {
-    res.send({ data: 'You will get quotes soon!' });
-}));
